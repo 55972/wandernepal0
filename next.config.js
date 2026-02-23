7312f3e62bdb1e0c2e6285ba2c2df16f5be90c75
55972 <baniyabibash8@gmail.com>
2026-02-23 18:31:46 +0545

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'ui-avatars.com' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+    formats: ['image/avif', 'image/webp'],
+  },
+  compress: true,
+  reactStrictMode: true,
+}
+
+module.exports = nextConfig
